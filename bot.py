801a60ba1882c3404a8695a5be8ee31a390015c7
posseidonnn <300918761+posseidonnn@users.noreply.github.com>
2026-09-02 21:50:18 +0530

    v1.3

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.fsm.storage.memory import MemoryStorage
 from config import BOT_TOKEN
 from config import ADMIN_IDS
